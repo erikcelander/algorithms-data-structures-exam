f81d58e3dc4d6d2ed17b1e41c7a9b69bf9f956e2
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-10-31 22:46:40 +0100

    update implementation logic

--- app/src/main/java/hemtentamen/PickNumberGame.java
+++ app/src/main/java/hemtentamen/PickNumberGame.java
@@ -2,19 +2,14 @@ package hemtentamen;
 
 public class PickNumberGame {
   public static void main(String[] args) {
-    int[] list = { 4, 1, 3, 2 };
-    System.out.println("Optimal move is: " + calculateOptimalFirstMove(list, 0, list.length - 1, true));
+    int[] list = { 4, 1, 3, 15 };
+    System.out.println("Optimal move is: " + calculateOptimalFirstMoveForTwoPlayers(list));
   }
 
-  /**
-   * recursively calculates the optimal move in the number picking game
-   *
-   * @param numbers - array of numbers to pick from
-   * @param first - first index in the array
-   * @param last - last index in the array
-   * @param isMyTurn - boolean to indicate whose turn it is
-   * @return - the value of the optimal move
-   */
+  private static int calculateOptimalFirstMoveForTwoPlayers(int[] list) {
+    return calculateOptimalFirstMove(list, 0, list.length - 1, true);
+  }
+
   private static int calculateOptimalFirstMove(int[] list, int first, int last, boolean isMyTurn) {
     // no numbers left to pick
     if (first > last) {
@@ -22,24 +17,24 @@ public class PickNumberGame {
     }
 
     if (isMyTurn) {
-      // calculate the score differences if the first or last number is picked for my turn
-      int scoreDifferenceIfFirstPicked = list[first] - calculateOptimalFirstMove(list, first + 1, last, false);
-      int scoreDifferenceIfLastPicked = list[last] - calculateOptimalFirstMove(list, first, last - 1, false);
+      int firstPick = list[first] - calculateOptimalFirstMove(list, first + 1, last, false);
+      int lastPick = list[last] - calculateOptimalFirstMove(list, first, last - 1, false);
 
-      // return the value of the optimal move i.e where we minimize the players score
-      if (scoreDifferenceIfFirstPicked < scoreDifferenceIfLastPicked) {
+      // return the value of the optimal move
+      if (firstPick < lastPick) {
         return list[first];
       } else {
         return list[last];
       }
 
     } else {
-      // calculate the score differences if the first or last number is picked for opponents turn
-      int scoreDifferenceIfFirstPicked = list[first] - calculateOptimalFirstMove(list, first + 1, last, true);
-      int scoreDifferenceIfLastPicked = list[last] - calculateOptimalFirstMove(list, first, last - 1, true);
+    
 
-      // return the largest score difference
-      return Math.max(scoreDifferenceIfFirstPicked, scoreDifferenceIfLastPicked);
+      int opponentFirstPick = list[first] - calculateOptimalFirstMove(list, first + 1, last, true);
+      int opponentLastPick = list[last] - calculateOptimalFirstMove(list, first, last - 1, true);
+
+      // opponent strategy is to move that is worst for me
+      return Math.max(opponentFirstPick, opponentLastPick);
     }
   }
 }
