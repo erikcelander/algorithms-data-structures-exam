36551d7c13df1b13ad04e5c5007f7a5bba65d3ef
Erik Kroon Celander <ek223ur@student.lnu.se>
2023-10-31 14:27:15 +0100

    init and create class for pick number game

---
+++ app/src/main/java/hemtentamen/PickNumberGame.java
@@ -0,0 +1,12 @@
+package hemtentamen;
+
+public class PickNumberGame {
+  public static void main(String[] args) {
+    int[] list = {4, 1, 3, 2};
+    System.out.println("Optimal move is: " + calculateOptimalMove(list, 0, list.length - 1, true));
+}
+
+  private static int calculateOptimalMove(int[] list, int start, int end, boolean isMyTurn) {
+    return 0;
+  }
+}
